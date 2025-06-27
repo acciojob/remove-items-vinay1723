cc9e859f973acac5ae520f915491c118da2a6c36
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-06-27 14:33:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+let formbutton = document.querySelector('input[type]');
+let select = document.getElementById('colorSelect');
+
+
+formbutton.addEventListener('click',(e) => {
+	e.preventDefault();
+	let options = select.options;
+	
+	for (let i = 0;i<options.length;i++) {
+		if (select.value === options[i].textContent) {
+			select.remove(i);
+			break;
+		}
+	}
+	
+})
